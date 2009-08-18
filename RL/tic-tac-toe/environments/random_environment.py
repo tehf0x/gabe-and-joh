d5abd05bad49724d3cf2f877dc3debadb5ab8d74
tehf0x <gabe@squirrelsoup.net>
2009-08-18 15:14:52 +0530

    Fixed up random environment.

--- RL/tic-tac-toe/environments/random_environment.py
+++ RL/tic-tac-toe/environments/random_environment.py
@@ -8,10 +8,10 @@ class RandomEnvironment(WrapperEnvironment):
         Pick the first free spot, and play there.
         """
         open_spots = []
-        for i in len(self.state):
+        for i in range(len(self.state)):
             if self.state[i] == 0:
-                open_spots.append[i]
+                open_spots.append(i)
         self.state[random.choice(open_spots)] = self.color
 
 if __name__ == "__main__":
-    EnvironmentLoader.loadEnvironment(SatelliteEnvironment())
+    EnvironmentLoader.loadEnvironment(RandomEnvironment())
