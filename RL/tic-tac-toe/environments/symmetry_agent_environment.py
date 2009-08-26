4c6f703fb38ee67d58710646fd67866f64950e63
tehf0x <gabe@squirrelsoup.net>
2009-08-26 17:59:03 +0530

    Added multi-line plot, and working agentVSagent code.

--- RL/tic-tac-toe/environments/symmetry_agent_environment.py
+++ RL/tic-tac-toe/environments/symmetry_agent_environment.py
@@ -4,6 +4,7 @@ from rlglue.environment import EnvironmentLoader
 
 class SymmetryEnvironment(WrapperEnvironment, SymmetryAgent):
     
+    player = 2
     def env_play(self): 
         action = self.do_step(self.state)
         self.state = action.intArray
