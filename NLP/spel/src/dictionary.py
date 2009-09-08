8583fe99cbed1817c92001ee4b75a9ec63ae88d1
tehf0x <gabe@squirrelsoup.net>
2009-09-08 11:14:03 +0530

    Working Dictionary class.

--- NLP/spel/src/dictionary.py
+++ NLP/spel/src/dictionary.py
@@ -1,5 +1,20 @@
 '''
+Dictionary Lookup
+
+Lookup a word in a specified dictionary to see if it is present.
+
 Created on Sep 7, 2009
 
-@author: garnold
+@author: Gabe Arnold <gabe@squirrelsoup.net>
 '''
+
+class DictLookup():
+    
+    def __init__(self, dict_path):
+        f = open(dict_path)
+        self.words = f.read().split()
+        
+    def is_word(self, word):
+        return self.words.count(word) == 1
+    
+
