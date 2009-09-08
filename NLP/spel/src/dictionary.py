f8bf162de4d3877877a26ca76c4a407777ff6b4e
tehf0x <gabe@squirrelsoup.net>
2009-09-08 18:06:33 +0530

    Combined dictionary.

--- NLP/spel/src/dictionary.py
+++ NLP/spel/src/dictionary.py
@@ -1,20 +1,24 @@
 '''
 Dictionary Lookup
 
-Lookup a word in a specified dictionary to see if it is present.
+Lookup a word in a dictionary to see if it is defined.
+The dictionary is composed of a specified text file that will be used to 
+complement the NLTK 'words' corpus.
 
 Created on Sep 7, 2009
 
 @author: Gabe Arnold <gabe@squirrelsoup.net>
 '''
 
+from nltk.corpus import words
+
 class Dictionary():
     
     def __init__(self, dict_path = '/etc/dictionaries-common/words'):
-        f = open(dict_path)
-        self.words = f.read().split()
+         f= open(dict_path)
+         self.words = set(f.read().split()).union(words.words())
         
     def has_word(self, word):
-        return self.words.count(word) == 1
+        return word in self.words
     
 
