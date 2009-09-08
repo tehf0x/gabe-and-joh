37d1276d0dba418f279c4ab66874a5339c414956
joh <joh@pseudoberries.com>
2009-09-08 18:23:35 +0530

    Add sort by word frequency capability.

--- NLP/spel/src/corpus.py
+++ NLP/spel/src/corpus.py
@@ -15,3 +15,16 @@ words = [w.lower() for w in brown.words()]
 """ Frequency distribution of words """
 freq = FreqDist(words)
 
+def freq_cmp(w1, w2):
+    f1 = freq[w1]
+    f2 = freq[w2]
+    if f1 < f2:
+        return 1
+    elif f1 == f2:
+        return 0
+    else: # f1 < f2
+        return -1
+
+def freq_sort(words):
+    """ Sort list of words by frequency in the corpus """
+    return sorted(words, freq_cmp)
