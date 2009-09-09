7d6beeab06034c0d4d00cca33ee172a110785ec3
tehf0x <gabe@squirrelsoup.net>
2009-09-09 18:17:41 +0530

    Added meta function in permutate module.

--- NLP/spel/src/permutate.py
+++ NLP/spel/src/permutate.py
@@ -8,7 +8,34 @@ Created on 8 Sep 2009
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-def permutate(word):
+def permutate_meta(word):
+    """
+    Get a set of all possible edits of word of distance 1, and return the
+    edit type along with the word.
+    """
+    # Split word into two in all possible ways
+    split = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    
+    # Deletes one character
+    deletes = [('delete', (a[-1] if len(a)>0 else '@', b[0]), a + b[1:]) 
+               for a, b in split if b]
+    
+    # Swaps two characters
+    transposes = [('transpose', (b[0], b[1]), a + b[1] + b[0] + b[2:]) 
+                  for a, b in split if len(b) > 1]
+    
+    # Replace character
+    replaces = [('replace', (b[0], c), a + c + b[1:]) 
+                for a, b in split for c in alphabet if b]
+    
+    # Insert character
+    inserts = [('insert', (a[-1] if len(a)>0 else '@', c), a + c + b) 
+               for a, b in split for c in alphabet]
+    
+    # Remove duplicates by creating a set
+    return set(deletes + transposes + replaces + inserts)
+
+def permutate(word, meta = False):
     """ Get a set of all possible edits of word of distance 1"""
     # Split word into two in all possible ways
     split = [(word[:i], word[i:]) for i in range(len(word) + 1)]
