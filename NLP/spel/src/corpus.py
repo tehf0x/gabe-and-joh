6080a5e863a9f5d1e325a75781be0db4eceb421f
joh <joh@pseudoberries.com>
2009-09-13 02:06:21 +0530

    General code and interface improvements: - Spellchecker now represents a spell checker with different options available. - corpus.FreqDist for word counting. Can store to a space separated file (faster than pickle)

--- NLP/spel/src/corpus.py
+++ NLP/spel/src/corpus.py
@@ -1,63 +1,52 @@
-'''
+"""
 Corpus based candidate matching and scoring
 
 Created on 8 Sep 2009
 
 @author: Johannes H. Jensen <joh@pseudoberries.com>
-'''
-
-import pickle
+"""
 
 import nltk
-from nltk import word_tokenize, FreqDist
-from nltk.corpus import brown
 
-sents = brown.sents()
-words = brown.words()
-
-def load(filename='corpus.pickle'):
-    """ Attempt to load corpus data from pickle """
-    try:
-        fh = open(filename, 'r')
-        #print 'Loading corpus from "%s"...' % (filename) 
-        return pickle.load(fh)
-        
-    except IOError:
-        # File not found, generate
-        #print 'Generating corpus to "%s"... This can take some time...' % (filename)
-        
-        # Lowercase the first words in sentences
-        '''sents = brown.sents()
-        
-        for i,s in enumerate(sents):
-            sents[i][0] = sents[i][0].lower()
-            words.extend(sents[i])
-        '''
-        words = [w.lower() for w in brown.words()]
-        freq = FreqDist(words)
-        
-        pickle.dump(freq, open(filename, 'w'))
-        
-        return freq
-
-# Load frequency distribution
-freq = load()
-
-
-
-def known(word_list):
-    return set(w for w in word_list if freq.has_key(w))
-
-def freq_cmp(w1, w2):
-    f1 = freq[w1]
-    f2 = freq[w2]
-    if f1 < f2:
-        return 1
-    elif f1 == f2:
-        return 0
-    else: # f1 < f2
-        return -1
-
-def freq_sort(words):
-    """ Sort list of words by frequency in the corpus """
-    return sorted(words, freq_cmp)
+class FreqDist(nltk.FreqDist):
+    """ Frequency distribution with some additional functionality """
+    
+    def load(self, file):
+        """ Load frequency counts from file """
+        self.loads(file.read())
+    
+    def loads(self, s):
+        for line in s.split("\n"):
+            if not line:
+                continue
+            word, count = line.split()
+            self[word] = int(count)
+    
+    def dumps(self):
+        s = ""
+        for word, count in self.items():
+            s += "%s\t%d\n" % (word, count)
+            
+        return s
+    
+    def dump(self, file):
+        """ Save frequency counts from file """
+        file.write(self.dumps())
+    
+    def known(self, word_list):
+        """ Return a subset of word_list where each element is known """
+        return set(w for w in word_list if self.has_key(w))
+    
+    def freq_cmp(self, w1, w2):
+        f1 = self[w1]
+        f2 = self[w2]
+        if f1 < f2:
+            return -1
+        elif f1 == f2:
+            return 0
+        else: # f1 < f2
+            return 1
+    
+    def sort_words(self, words, reverse=True):
+        """ Sort list of words by frequency in the corpus """
+        return sorted(words, cmp=self.freq_cmp, reverse=reverse)
