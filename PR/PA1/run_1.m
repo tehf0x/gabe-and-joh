f2ac7fac5412369b391fe6248b069fa98baa49f2
joh <joh@pseudoberries.com>
2009-09-17 18:56:31 +0530

    Tex for producing the figures.

--- PR/PA1/run_1.m
+++
@@ -1,8 +0,0 @@
-% Load the dataset
-dataset = load('Datasets/ls_group14.txt');
-
-% Offsets of class data in dataset
-class_offsets = [500, 1000, 1500];
-
-% Run the tests
-run_tests(dataset, class_offsets)
