61ae8ff050af4109a313c05eaa2d9aa9bd68084f
joh <joh@pseudoberries.com>
2009-09-25 18:51:13 +0530

    Added policy iteration to experiment.

--- RL/herd/experiment.py
+++ RL/herd/experiment.py
@@ -8,13 +8,26 @@ File that actually runs the experiments.
 '''
 
 
-from value_methods import value_iteration
+from value_methods import value_iteration, policy_iteration
 from graph_gen import graph_vals
+
+#
+# Value Iteration
+#
 
 v1 = value_iteration(sweeps=1)
 v10 = value_iteration(sweeps=10)
 v_f = value_iteration(theta=0.01)
 
-graph_vals(v1, 'One Iteration')
-graph_vals(v10, 'Ten Iterations')
-graph_vals(v_f, 'Convergence')
+graph_vals(v1, 'Sweep 1')
+graph_vals(v10, 'Sweep 10')
+graph_vals(v_f, 'Optimal V*')
+
+#
+# Policy Iteration
+#
+values = []
+V, pi = policy_iteration(theta=0.01, value_list=values)
+
+for i, v in enumerate(values):
+    graph_vals(v, 'Policy Evaluation %d' % (i + 1))
