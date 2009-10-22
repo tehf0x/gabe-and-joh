bf4286434fa0af16fde2173c537e30b50a4b6164
tehf0x <gabe@squirrelsoup.net>
2009-10-22 19:30:18 +0530

    Fixed eval.

--- PR/PA2/eval_results.m
+++ PR/PA2/eval_results.m
@@ -4,8 +4,8 @@ function [accuracy, confusion] = eval_results(test_data, classified_data)
     num_classes = length((test_data));
     accuracies = {};
     for i=1:num_classes
-        num_right = size(intersect(test_data{i}, classified_data{i}, 'rows'), 1);
-        accuracies{i} = num_right / length(test_data{i});
+        num_wrong = size(setdiff(test_data{i}, classified_data{i}, 'rows'), 1);
+        accuracies{i} = 1 - num_wrong / length(test_data{i});
     end
     
     %This syntax is ugly, but oh well...
@@ -14,6 +14,6 @@ function [accuracy, confusion] = eval_results(test_data, classified_data)
     confusion = zeros(num_classes);
     for i=1:num_classes
        for j=1:num_classes
-           confusion(i,j) = size(intersect(test_data{i}, classified_data{j}, 'rows'), 1);
+           confusion(i,j) = length(test_data{i}) - size(setdiff(test_data{i}, classified_data{j}, 'rows'), 1);
     end
 end
