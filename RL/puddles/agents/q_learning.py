70796868508af20350a8cec5fa5b0406cc2d2705
tehf0x <gabe@squirrelsoup.net>
2009-10-29 14:23:58 +0530

    Changed to a base agent class which is extended into Q-learning and Sarsa.

--- RL/puddles/agents/q_learning.py
+++ RL/puddles/agents/q_learning.py
@@ -11,37 +11,14 @@ Agent settings can be modified real-time by sending messages to the
 agent through RL-Glue. The format is <setting>=<value>
 """
 
-import re
-import random
-from copy import copy
-
-from rlglue.agent.Agent import Agent
+from td_agent import TDAgent
 from rlglue.agent import AgentLoader as AgentLoader
-from rlglue.types import Action
-from rlglue.types import Observation
 
 
-class QAgent(Agent):    
+class QAgent(TDAgent):    
     """
     Implements Q-Learning in an agent.
     """
-    
-    #The 4 actions we can take.
-    actions = ['E', 'N', 'S', 'W']
-    
-    #Some constants
-    #Discount Gamma value
-    gamma = 0.9
-    #Q-Update Alpha Value
-    alpha = 0.8
-    #Epsilon-Greed epsilon value
-    epsilon = 0.9
-    #Initialize Q
-    Q = {}
-    
-    #We need to remember our last action and state for updating Q
-    last_state = []
-    last_action = []
     
     def update_Q(self, state, action, reward, new_state):
         """
@@ -64,78 +41,6 @@ class QAgent(Agent):
         #The famous formula:
         Q = Q + self.alpha * (reward + self.gamma * Qp - Q)
         self.Q[state][action] = Q
-        
-    def policy(self, state):
-        """
-        Return the action to be taken for the state given.
-        """
-        #Greedy policy means pick the best action!
-        try:
-            action = max(self.Q[state].values())
-        except (KeyError, ValueError):
-            return random.choice(self.actions)
-        
-        #Epsilon-greedy decision:
-        if(random.uniform(0, 1) >= self.epsilon):
-            tmp_actions = copy(self.actions)
-            tmp_actions.remove(action)
-            return random.choice(tmp_actions)
-        else:
-            return action
-    
-    def do_step(self, state, reward = False):
-        """
-        Runs the actual Q-Learning algorithm.
-        In a separate function so it can be called both on start and on step.
-        """
-        a_obj = Action()
-        #Query the policy to find the best action
-        #a_obj.intArray = self.policy(state)
-        a_obj.charArray = [self.policy(state)]
-        #Run the Q update if this isn't the first step
-        if(reward):
-            self.update_Q(self.last_state, self.last_action, reward, state)
-        #Save the current state-action pair for the next step's Q update.
-        self.last_state = state
-        self.last_action = list(a_obj.intArray)
-        #And we're done
-        return a_obj
-    
-    def agent_init(self, task_spec):
-        """Re-initialize the agent for a new training round."""
-        #Reset the Q-values
-        self.Q = {}
-    
-    def agent_start(self, state):
-        """ Called every time a new game is started """
-        state = tuple(state.intArray)
-        return self.do_step(state)
-    
-    def agent_step(self, reward, state):
-        """ Called for each game step """
-        state = tuple(state.intArray)
-        return self.do_step(state)
-
-    
-    def agent_end(self, reward):
-        """ Called when a game ends, this is where we learn """
-          
-    def agent_cleanup(self):
-        """ Clean up for next run """
-        
-    def agent_message(self, msg):
-        """ Retrieve message from the environment in the form param=value """
-        result = re.match('(.+)=(.+)', msg)
-        if result:
-            param, value = result.groups()
-            if param == 'echo' and value!= 'None':
-                return value
-            else:
-                return "Unknown parameter: " + param
-            
-        else:
-            return "Unknown command: " + msg;
-
 
 if __name__=="__main__":
     AgentLoader.loadAgent(QAgent())
