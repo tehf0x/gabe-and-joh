718fba47f429f8d8b662a7a0e438e43d18df26d8
joh <joh@pseudoberries.com>
2009-11-12 17:30:27 +0530

    Fixed

--- NLP/20ng/src/test_classifier.py
+++ NLP/20ng/src/test_classifier.py
@@ -7,16 +7,16 @@ import logging
 
 logger = logging.getLogger('test-classifier')
 
-def test_classifier(classifier, corpus, test_files, ex_slice, num_slices=10):
+def test_classifier(classifier, corpus, test_files):
     """
     classifier = instance of nltk.classify.ClassifierI
     corpus = instance of nltk.corpus.reader.plaintext.CategorizedCorpusReader
     """
     # Initialize confusion matrix
     labels = classifier.labels()
-    confusion = dict((l, dict((l, 0) for l in labels)) for l in labels)
+    confusion = dict((l, dict((l, 0) for l in labels)) for l in test_files.keys())
 
-    for label, files in test_files.item():
+    for label, files in test_files.items():
         for fileid in files:
             l = classifier.classify(corpus.words(fileids=[fileid]))
             confusion[label][l] += 1
