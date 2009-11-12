178eb9d81e5a52482f4b76cea2d1b232fc54d6bc
tehf0x <gabe@squirrelsoup.net>
2009-11-12 16:45:06 +0530

    Added accuracy calculation.

--- NLP/20ng/src/test_classifier.py
+++ NLP/20ng/src/test_classifier.py
@@ -15,14 +15,27 @@ def test_classifier(classifier, corpus):
     # Initialize confusion matrix
     labels = classifier.labels()
     confusion = dict((l, dict((l, 0) for l in labels)) for l in labels)
-    
+
     for label in corpus.categories():
         for fileid in corpus.fileids(categories=[label]):
             l = classifier.classify(corpus.words(fileids=[fileid]))
             confusion[label][l] += 1
             logger.info(label + ': ' + fileid + ' => ' + l + '!')
-    
+
     return confusion
+
+def calc_accuracy(confusion):
+    '''
+    Calculate the accuracy froma confusion matrix.
+    '''
+
+    accuracy = 0.0
+
+    for l,r in confusion.items():
+        accuracy += float(r[l]) / sum(r.values())
+
+    return accuracy / len(confusion)
+
 
 if __name__ == '__main__':
     pass
