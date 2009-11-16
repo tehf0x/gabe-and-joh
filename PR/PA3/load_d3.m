37ed5934f58f11aaa4e9bc53a4a3c338f79f758a
tehf0x <gabe@squirrelsoup.net>
2009-11-16 11:34:08 +0530

    All load data scripts.

---
+++ PR/PA3/load_d3.m
@@ -0,0 +1,30 @@
+%Run the tests for the bayesian classifier on the 2d linearly seperable
+%dataset
+dataset = load('datasets/od_group14.txt');
+class_offsets = [500,1000,1500]
+
+% Split data into N classes
+n_classes = size(class_offsets, 2);
+class_data = {};
+offset = 1;
+for i=1:n_classes
+    class_data{i} = dataset(offset:class_offsets(i),:);
+    offset = 1 + class_offsets(i);
+end
+
+n_classes = 3;
+% Grab the lower 75% of each class for training data
+training_data = {};
+for i=1:n_classes
+    % Get number of elements in class:
+    lim = floor(size(class_data{i}, 1) * 0.75);
+    training_data{i} = class_data{i}(1:lim, :);
+end
+   
+% Use 25% of data for testing
+test_data = {};
+for i=1:n_classes
+    % Get number of elements in class:
+    lim = floor(size(class_data{i}, 1) * 0.75);
+    test_data{i} = class_data{i}(lim+1:end, :);
+end
